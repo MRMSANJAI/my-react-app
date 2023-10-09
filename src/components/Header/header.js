3a2b15b53f917daa2c6cf359884aae3d3aae9174
yoganathanp <109456314+yoganathanp@users.noreply.github.com>
2023-10-09 15:58:15 +0530

    kl

---
+++ src/components/Header/header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+const Header = () => {
+    return(
+        <div style={{backgroundColor:'red'}}>
+            <h1>Nav</h1>
+            <h1>Nav</h1>
+            <h1>Nav</h1>
+        </div>
+    )
+}
+
+export default Header
