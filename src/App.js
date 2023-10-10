903caa64df53ef630aafc26aac163bcdc52fca92
yoganathanp <109456314+yoganathanp@users.noreply.github.com>
2023-10-10 10:28:03 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route } from "react-router-dom";
 import './App.css';
-import Header from './components/Header/header';
+import Home from "./components/Home/home";
 
- const App = ()=>{
-
-  return(
-<Header/>
-  )
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Route path="/home" element={<Home />} />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
